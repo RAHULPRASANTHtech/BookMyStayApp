aa58c29eb36d59053a756437d15711020e678231
RAHULPRASANTHtech <ranga.asdfgf@gmail.com>
2026-03-24 15:50:01 +0530

    Rahul Added UC1-WelcomePage with proper structure

--- BookMyStayApp/src/UseCase1HotelBookingApp.java
+++ BookMyStayApp/src/UseCase1HotelBookingApp.java
@@ -1,8 +1,8 @@
 public class UseCase1HotelBookingApp{
     public static void main (String[]args){
         System.out.println("=====================");
-        System.out.println("Welcome to Back My Stay App");
-        System.out.println("Hotel Booking System v1.0 ");
+        System.out.println("Welcome to Hotel Booking Management System");
+        System.out.println("System Initialized Successfully - Hotel Booking System v1.0 ");
         System.out.println("=====================");
     }
 }
