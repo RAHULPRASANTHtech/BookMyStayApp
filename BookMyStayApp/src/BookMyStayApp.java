03d301ec7082aa0addac04901c216d71d705425c
RAHULPRASANTHtech <ranga.asdfgf@gmail.com>
2026-03-24 16:09:08 +0530

    Added UC3 Centralized Room Inventory

--- BookMyStayApp/src/BookMyStayApp.java
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+
+// Abstract Room Class
 abstract class Room {
     protected int beds;
     protected int size;
@@ -16,55 +19,80 @@ abstract class Room {
     }
 }
 
-// Single Room
+// Room Types
 class SingleRoom extends Room {
     public SingleRoom() {
         super(1, 250, 1500.0);
     }
 }
 
-// Double Room
 class DoubleRoom extends Room {
     public DoubleRoom() {
         super(2, 400, 2500.0);
     }
 }
 
-// Suite Room
 class SuiteRoom extends Room {
     public SuiteRoom() {
         super(3, 750, 5000.0);
     }
 }
 
+// 🔥 NEW: Inventory Class (UC3 Core)
+class RoomInventory {
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+
+        // Initialize availability
+        inventory.put("Single", 5);
+        inventory.put("Double", 3);
+        inventory.put("Suite", 2);
+    }
+
+    // Get availability
+    public int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
+    }
+
+    // Update availability
+    public void updateAvailability(String type, int count) {
+        inventory.put(type, count);
+    }
+
+    // Display all inventory
+    public void displayInventory(Room single, Room dbl, Room suite) {
+
+        System.out.println("Hotel Room Inventory Status\n");
+
+        System.out.println("Single Room:");
+        single.displayDetails();
+        System.out.println("Available Rooms: " + getAvailability("Single") + "\n");
+
+        System.out.println("Double Room:");
+        dbl.displayDetails();
+        System.out.println("Available Rooms: " + getAvailability("Double") + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayDetails();
+        System.out.println("Available Rooms: " + getAvailability("Suite"));
+    }
+}
+
+// Main Class
 public class BookMyStayApp {
     public static void main(String[] args) {
 
-        System.out.println("Hotel Room Initialization\n");
-
-        // Create room objects (Polymorphism)
+        // Create room objects (same as UC2)
         Room single = new SingleRoom();
         Room dbl = new DoubleRoom();
         Room suite = new SuiteRoom();
 
-        // Static availability variables
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        // 🔥 Use centralized inventory
+        RoomInventory inventory = new RoomInventory();
 
-        // Display Single Room
-        System.out.println("Single Room:");
-        single.displayDetails();
-        System.out.println("Available: " + singleAvailable + "\n");
-
-        // Display Double Room
-        System.out.println("Double Room:");
-        dbl.displayDetails();
-        System.out.println("Available: " + doubleAvailable + "\n");
-
-        // Display Suite Room
-        System.out.println("Suite Room:");
-        suite.displayDetails();
-        System.out.println("Available: " + suiteAvailable);
+        // Display inventory
+        inventory.displayInventory(single, dbl, suite);
     }
 }
