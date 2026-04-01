f48bf35007b7f5bb76b76daf26538f549cd6acb6
RAHULPRASANTHtech <ranga.asdfgf@gmail.com>
2026-04-01 14:25:28 +0530

    Updated UC10 Cancellation + cleaned old files

--- src/UseCase1HotelBookingApp.java
+++
@@ -1,9 +0,0 @@
-public class UseCase1HotelBookingApp{
-    public static void main (String[]args){
-       System.out.println("=====================");
-       System.out.println("Welcome to Hotel Booking Management System");
-       System.out.println("System Initialized Successfully - Hotel Booking System v1.0 ");
-
-       System.out.println("=====================");
-    }
-}
